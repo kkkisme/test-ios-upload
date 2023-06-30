492343e64eae3ca59332692763efe7adf4a9a9ea
huming <huming@linshimuye.com>
2023-06-30 19:26:25 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+const handleUpload = () => {
+  navigator.geolocation.getCurrentPosition((res) => {
+    console.log(res)
+  }, (err) => {
+    console.log(err)
+  })
+}
+</script>
+
+<template>
+  <van-uploader @click-upload="handleUpload" capture="true">
+
+  </van-uploader>
+</template>
+
+<style scoped>
+
+</style>
